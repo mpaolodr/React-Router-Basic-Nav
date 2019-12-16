bae4fb76e29aeda4f09838751ef6f7c7915ae326
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-16 15:19:47 -0500

    imported BrowserRouter and created Route component on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+
     <Navigation />
   </div>
 );
